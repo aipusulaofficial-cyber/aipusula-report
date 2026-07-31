2610a0b3e60fe507d1af6e12a0e7c92382058177
aipusulaofficial-cyber <aipusulaofficial@gmail.com>
2026-07-31 15:43:07 +0300

    Reorder NotFound import and route declaration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
+
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
+
 import Home from "./pages/Home";
 import AIWorld from "./pages/AIWorld";
 import AITools from "./pages/AITools";
@@ -12,6 +13,8 @@ import DigitalWorld from "./pages/DigitalWorld";
 import CyberSecurity from "./pages/CyberSecurity";
 import ContentDetail from "./pages/ContentDetail";
 import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
+
 function Router() {
   return (
     <Switch>
@@ -22,13 +25,14 @@ function Router() {
       <Route path="/dijital-dunya" component={DigitalWorld} />
       <Route path="/siber-guvenlik" component={CyberSecurity} />
       <Route path="/detay/:category/:type/:slug" component={ContentDetail} />
-      <Route path="/404" component={NotFound} /><Route path="/admin" component={Admin} />
+      <Route path="/admin" component={Admin} />
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
 }
 
-function App() {
+export default function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="dark" switchable>
@@ -40,5 +44,3 @@ function App() {
     </ErrorBoundary>
   );
 }
-
-export default App;
