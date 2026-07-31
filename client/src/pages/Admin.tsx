40693fc5c2ebd6463f68e377f9e463bfbb9aa4df
aipusulaofficial-cyber <aipusulaofficial@gmail.com>
2026-07-31 15:32:11 +0300

    Update Admin.tsx

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,1 +1,12 @@
+import { useState } from "react";
 
+export default function Admin() {
+  // form burada olacak
+
+  return (
+    <div>
+      <h1>AIPUSULA Admin Paneli</h1>
+      ...
+    </div>
+  );
+}
